c953febacd4356a89dbdca44334fb24542a28aa1
Peer <8089487+peerhenry@users.noreply.github.com>
2026-07-09 20:52:05 +0200

    add tailwind

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './app.vue',
+    './plugins/**/*.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
